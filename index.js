96a556a02ceb4ec274807b1dd0e4b8d4d14812b4
SK386 <caiorichateb@gmail.com>
2023-01-24 13:21:32 -0300

    update

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const app = express();
-const handlebars = require('handlebars')
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const veng = require('ejs')
 
 //bodyparser
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({extended: true}));
 
 //view engine ejs
-app.set("view engine", "ejs");
+app.engine("html", require('ejs').renderFile);
+app.set("view engine", "html");
+app.set("views", "src/views/");
 // css e js
 app.use(express.static('src/public'))
-
+app.use(favicon(__dirname + '/src/public/imagens/favicon.ico'));
+//rotas
 app.get("/", function(req, res){
-	res.sendFile(__dirname + "/src/views/index.html")
+	res.render(__dirname + "/src/views/index.html")
 })
 app.get("/planos", function(req, res){
-	res.sendFile(__dirname + "/src/views/planos.html")
+	res.render(__dirname + "/src/views/planos.html")
 })
 app.get("/login", function(req, res){
 	res.sendFile(__dirname + "/src/views/login.html")
 })
 app.get("/post", function(req, res){
-	res.sendFile(__dirname + "/src/views/post.html")
+	res.render(__dirname + "/src/views/post.html")
 })
 app.get("/register", function(req, res){
 	res.sendFile(__dirname + "/src/views/register.html")
@@ -31,8 +35,13 @@ app.get("/reset", function(req, res){
 	res.sendFile(__dirname + "/src/views/reset.html")
 })
 app.get("/contato", function(req, res){
-	res.sendFile(__dirname + "/src/views/contato.html")
+	res.render(__dirname + "/src/views/contato.html")
 })
+
+app.get("/sobre", function(req, res){
+	res.render(__dirname + "/src/views/sobre.html")
+})
+
 app.listen(7470, function(){
 	console.log("rodando em http://localhost:7470")
 })
